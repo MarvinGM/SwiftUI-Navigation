d55bfcd2fcff8646c3ac78ea5ede5083fdf39cad
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 14:46:26 +0800

    Version 1.0

--- SwiftUI Navigation/ContentView.swift
+++ SwiftUI Navigation/ContentView.swift
@@ -9,8 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Screen(title: "Home",
+                   text: "First Screen",
+                   imageName: "house")
+        }
+    }
+}
+
+struct Screen: View {
+    
+    let title: String
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200, alignment: .center)
+                .padding()
+            
+            Text(text)
+                .font(.system(size: 30, weight: .light, design: .default))
+                .padding()
+            
+            NavigationLink(
+                destination: Screen(title: "Notifications", text: "Second Screen", imageName: "bell"),
+                label: {
+                    ContinueButton(color: .pink)
+                }
+            )
+        }
+        .navigationTitle(title)
+    }
+}
+
+
+struct ContinueButton: View {
+    
+    let color: Color
+    
+    var body: some View {
+        Text("Continue")
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(color)
+            .foregroundColor(.white)
+            .cornerRadius(8)
     }
 }
 
